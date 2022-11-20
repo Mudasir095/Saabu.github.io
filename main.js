7f9109cff0fabfc227918688f05f080c11b49787
Mudasir095 <117685172+Mudasir095@users.noreply.github.com>
2022-11-20 21:13:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+// var element=document.getElementById("bod");
+// var textel=document.getElementsByClassName("info")
+// var count=0;
+// function ok()
+// {
+//     if(count%2==0)
+//     {
+
+//     element.style.backgroundColor="black";
+//     // text.style.backgroundColor="white";
+//     // textel.style.backgroundColor="white";
+//     }
+//     else{
+//         element.style.backgroundColor="white";
+//         // textel.style.backgroundColor="black";
+//     }
+//     count++;
+// }
